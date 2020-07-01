5b24642fbff1756764b4d857f302d3fecbb826ac
Michał Goliński <golinski@amu.edu.pl>
2020-07-01 23:19:37 +0200

    Update fetch.js

--- src/core/request/fetch.js
+++ src/core/request/fetch.js
@@ -95,6 +95,7 @@ function fetchRequest(args) {
           delete transformedRequest.url;
           return fetch(url || finalURL, transformedRequest)
             .then((res) => {
+              console.log(res)
               if (res.status >= 500) {
                 return reject(res);
               }
